6d6198f94656de821f102965ee30ceb40f882625
sriram23 <sriramsubramanian23@gmail.com>
2017-04-24 16:56:55 +0530

    Added PrintMultiple Language

---
+++ printTamil/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello World!"); //English
+    println!("ஹலோ உலகம்!"); //Tamil
+    println!("नमस्ते दुनिया!"); //Hindi
+    println!("హలో వరల్డ్!"); //Telugu
+    println!("ഹലോ വേൾഡ്!"); //Malayalam
+    println!("你好，世界！"); //Chinese
+    println!("Привет мир!"); //Russian
+    println!("¡Hola Mundo!"); //Spanish
+    println!("မင်္ဂလာပါကမ္ဘာလောက!"); //Burmese
+}
